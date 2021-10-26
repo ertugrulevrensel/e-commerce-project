5f7c897d05ffb5f4dba8533f525d992085f9c6ee
Ertuğrul Evrensel <ertugrulevrensel@gmail.com>
2021-10-27 00:40:46 +0300

    Fix useEffect bug

--- src/Pages/addProduct/index.js
+++ src/Pages/addProduct/index.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 import "./AddProduct.scss";
 import Header from "../../Components/header";
 import fail from "../../Assets/fail.webp";
